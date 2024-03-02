172fd93eab4845c638a12225243e709ee5611a7b
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2024-03-01 21:56:31 -0300

    Configurando rotas

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Abriu o arquivo');
+// Pegando o express
+const express = require('express')
+const app = express()
+
+// Criando rota
+app.get('/', function (req, res) {
+    res.send('Chegou a resposta')
+})
+
+app.get('/hello', function (req, res) {
+    res.send('Chegou a resposta hello')
+})
+
+app.get('/nome', function(res, res) {
+   res.send('Mateus')
+})
+
+// Ativando o servidor
+app.listen(3000, function () {
+    console.log('Server up')
+})
+
