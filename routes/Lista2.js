3bc8e59d25dead6bcc0267e79df0c35034113435
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2024-03-13 21:56:59 -0300

    iniciando Lista 4

--- routes/Lista2.js
+++ routes/Lista2.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/ex1', (req, res) => {
-    const { nota1, nota2, nota3, nota4 } = req.body;
-    const media = (nota1 + nota2 + nota3 + nota4) / 4;
-    const mensagem = media >= 7 ? 'Aprovado' : 'Reprovado';
-    res.json({ media, mensagem });
-});
+router.post('/ex1', function (req, res) {
+    res.json({})
+})
+
+module.exports = router
