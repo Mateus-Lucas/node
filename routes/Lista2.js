935fa9b7ae8447553cbae62c0b61301b137e3f4e
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2024-03-18 22:47:20 -0300

    resolvido ate ex3 - lista 2

--- routes/Lista2.js
+++ routes/Lista2.js
@@ -2,7 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/ex1', function (req, res) {
-    res.json({})
+    const { quantMinima, quantMaxima} = req.body;
+    const estoqueMedio = (quantMinima + quantMaxima) / 2;
+    res.json({ estoqueMedio });
+})
+
+router.post('/ex2', function (req, res) {
+    const { nomeFunc, horasTrabalhadas, valorHora, quantFilhos} = req.body;
+    const salarioBruto = (horasTrabalhadas * valorHora);
+    const extraFilhos = quantFilhos * (salarioBruto * 0.03);
+    const salarioFinal = salarioBruto + extraFilhos;
+    res.json({ nomeFunc, salarioFinal });
+})
+
+router.post('/ex3', function (req, res) {
+    const {nome, anos, meses, dias} = req.body;
+    anosDias = anos * 365;
+    mesesDias = meses * 30;
+    idade = anosDias + mesesDias + dias
+    res.json({ nome, idade });
 })
 
 module.exports = router
